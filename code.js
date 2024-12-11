b59887afdcf93a07e5b7d29e9311519567455aef
Amy Pieterse <amypieterse.15@gmail.com>
2024-12-11 02:52:27 +0200

    homepage

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
+
+const slider = document.querySelector('.slider')
+
 window.addEventListener('scroll', function(){
     if (window.scrollY > 0){
         document.body.classList.add('scroll');
@@ -5,3 +8,32 @@ window.addEventListener('scroll', function(){
         document.body.classList.remove('scroll');
     }
 });
+
+let sliderGrabbed = false;
+
+
+slider.addEventListener('mousedown', (e)=>{
+    sliderGrabbed=true;
+    slider.style.cursor = 'grabbing';
+})
+
+slider.addEventListener('mouseup', (e)=>{
+    sliderGrabbed=false;
+    slider.style.cursor = 'grab';
+})
+
+slider.addEventListener('mouseleave', (e)=>{
+    sliderGrabbed=false;
+    slider.style.cursor = 'grab';
+})
+
+
+slider.addEventListener('mousemove', (e)=>{
+    if (sliderGrabbed){
+        slider.parentElement.scrollLeft-=e.movementX; 
+    }
+})
+
+
+
+
